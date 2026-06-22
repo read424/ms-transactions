1f51e167b4f18f024b53ab57429145b2b3060fcf
read424 <read424@gmail.com>
2026-06-22 09:13:20 -0500

    chore: update application configuration

--- src/main/java/com/bootcamp/ms_transaction/MsTransactionApplication.java
+++ src/main/java/com/bootcamp/ms_transaction/MsTransactionApplication.java
@@ -2,8 +2,10 @@ package com.bootcamp.ms_transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsTransactionApplication {
 
 	public static void main(String[] args) {
